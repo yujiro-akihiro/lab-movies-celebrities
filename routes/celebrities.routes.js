abc0f767fe45fef94733e44ad89461137b969440
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-06-24 09:01:35 +0200

    #4 Iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,32 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const Celebrity = require('../models/Celebrity.model');
 
-// all your routes here
+// /celebrities/create GET route
+router.get('/create', (req, res) => {
+    res.render('celebrities/new-celebrity');
+});
+
+// /celebrities/create POST route
+router.post('/create', async (req, res) => {
+    const { name, occupation, catchPhrase } = req.body;
+
+    try {
+        await Celebrity.create({ name, occupation, catchPhrase });
+        res.redirect('/celebrities'); // このルートは次のイテレーションで作成します
+    } catch (error) {
+        res.render('celebrities/new-celebrity', { errorMessage: 'Error while creating the celebrity. Please try again.' });
+    }
+});
+
+// /celebrities GET route
+router.get('/', async (req, res) => {
+    try {
+        const celebrities = await Celebrity.find();
+        res.render('celebrities/celebrities', { celebrities });
+    } catch (error) {
+        console.log('Error while retrieving celebrities:', error);
+        res.render('error', { errorMessage: 'Error while retrieving celebrities.' });
+    }
+});
 
 module.exports = router;
